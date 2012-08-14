47e7ae2bcfcde636d4eb7593497cd1794bfba82d
Michal Ludvig <mludvig@logix.net.nz>
2012-08-14 15:14:26 +1200

    Support Brother scanner

--- scan2pdf.sh
+++ scan2pdf.sh
@@ -1,10 +1,19 @@
 #!/bin/bash -e
 
 ## Put your sane-detected device name here.
-DEVICE="snapscan"
+#DEVICE="snapscan"
+DEVICE='brother4:net1;dev0'
 
 ## For network scanners use
 #DEVICE="net:sane.example.org:snapscan"
+
+## See scanimage --device $(DEVICE) --help
+#SOURCE="FlatBed"
+SOURCE="Automatic Document Feeder(centrally aligned)"
+#SOURCE="Automatic Document Feeder(centrally aligned,Duplex)"
+
+#SCANIMAGE_OPTS=' --resolution 150 --brightness 20 --contrast 20 -l 0 -t 0 -x 210 -y 290'
+SCANIMAGE_OPTS=' --resolution 150 -l 0 -t 0 -x 210 -y 290'
 
 if [ "$1" = "" ]; then
         echo "Usage: $0 <output-file-name.pdf>"
@@ -19,8 +28,10 @@ echo "Temporary files kept in: ${TMPDIR}"
 
 cd ${TMPDIR}
 
-SCANIMAGE_OPTS="--device ${DEVICE} --mode Gray --resolution 150 --brightness 20 --contrast 20 -x 210 -y 290"
-scanimage ${SCANIMAGE_OPTS} --progress --verbose --batch  --batch-prompt --format=tiff
+set +e
+scanimage --device ${DEVICE} ${SCANIMAGE_OPTS} --source="${SOURCE}" --mode="True Gray" --progress --verbose --format=tiff --batch  # --batch-prompt
+set -e
+ls -1 out*.tif > /dev/null
 
 tiffcp -c lzw out*.tif scan.tiff
 
